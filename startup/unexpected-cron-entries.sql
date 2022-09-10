763b9eaed62e103e304a0a2654f32f26a321020c
Thomas Stromberg <t+github@chainguard.dev>
2022-09-10 07:56:40 -0400

    Add crontab query

---
+++ startup/unexpected-cron-entries.sql
@@ -0,0 +1,8 @@
+SELECT * FROM crontab
+WHERE command NOT LIKE "root%run-parts%"
+AND command NOT LIKE "%freshclam%"
+AND command NOT LIKE "%clamscan%"
+AND command NOT LIKE "%e2scrub%"
+AND command NOT LIKE "%zfs-linux%"
+AND command NOT LIKE "%anacron start%"
+AND command NOT LIKE "%/usr/lib/php/sessionclean%"
