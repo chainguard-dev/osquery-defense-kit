87f56088242a69a4ac75940bd833f582cc23b920
Thomas Stromberg <t+github@chainguard.dev>
2022-09-20 09:47:52 -0400

    Add more data to privesc, rewrite systemd units

--- process/unexpected-privilege-escalation.sql
+++ process/unexpected-privilege-escalation.sql
@@ -1,15 +1,24 @@
-SELECT p.pid,
-    p.path,
-    p.name,
-    p.cmdline,
-    p.euid,
-    p.parent,
+SELECT p.pid AS child_pid,
+    p.path AS child_path,
+    p.name AS child_name,
+    p.cmdline AS child_cmdline,
+    p.euid AS child_euid,
+    p.state AS child_state,
+    file.mode AS child_mode,
+    hash.sha256 AS child_hash,
+    p.parent AS parent_pid,
     pp.path AS parent_path,
     pp.name AS parent_name,
     pp.cmdline AS parent_cmdline,
-    pp.euid AS parent_euid
+    pp.euid AS parent_euid,
+    pfile.mode AS parent_mode,
+    hash.sha256 AS parent_hash
 FROM processes p
     JOIN processes pp ON p.parent = pp.pid
+    LEFT JOIN file ON p.path = file.path
+    LEFT JOIN hash ON p.path = hash.path
+    LEFT JOIN file AS pfile ON pp.path = file.path
+    LEFT JOIN hash AS phash ON pp.path = hash.path
 WHERE p.euid < pp.euid
     AND p.path NOT IN (
         '/usr/bin/fusermount',
