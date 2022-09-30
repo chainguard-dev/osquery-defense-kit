bb496d891663d109cae41620d1f652a784de6391
Thomas Stromberg <t+github@chainguard.dev>
2022-09-30 11:14:20 -0400

    Add kworker->modprobe exception

--- process/parent-missing-from-disk.sql
+++ process/parent-missing-from-disk.sql
@@ -7,6 +7,7 @@ SELECT p.name AS child_name,
   p.cmdline AS child_cmd,
   p.uid AS child_uid,
   p.gid AS child_gid,
+  hash.path,
   p.on_disk AS child_on_disk,
   pp.pid AS parent_pid,
   pp.name AS parent_name,
@@ -17,6 +18,7 @@ SELECT p.name AS child_name,
   pp.gid AS parent_gid
 FROM processes p
   JOIN processes pp ON pp.pid = p.parent
+  LEFT JOIN hash ON p.path = hash.path
 WHERE parent_on_disk != 1
   AND child_on_disk = 1
   AND NOT child_pid IN (1, 2)
@@ -39,3 +41,7 @@ WHERE parent_on_disk != 1
   )
   AND parent_path NOT LIKE "/app/extra/%"
   AND parent_path NOT LIKE "/opt/homebrew/Cellar/%"
+  AND NOT (
+    parent_name LIKE "kworker/%+events_unbound"
+    AND child_name IN ("modprobe")
+  )
