4c2767a0d70abdd3d188483f857a7b72af731d50
Thomas Stromberg <t+github@chainguard.dev>
2022-10-05 08:36:35 -0400

    Include /home and /Users

--- process/hidden-cwd.sql
+++ process/hidden-cwd.sql
@@ -10,18 +10,32 @@ SELECT p.pid,
   pp.cmdline AS parent_cmdline,
   pp.cwd AS parent_cwd,
   pp.euid AS parent_euid,
-  hash.sha256
+  hash.sha256,
+  REPLACE(p.cwd, u.directory, "~") AS dir,
+  CONCAT(
+    p.name,
+    ",",
+    IIF(
+      REGEX_MATCH(
+        REPLACE(p.cwd, u.directory, "~"),
+        "([/~].*?/.*?/.*?)/",
+        1
+      ) != "",
+      REGEX_MATCH(
+        REPLACE(p.cwd, u.directory, "~"),
+        "([/~].*?/.*?/.*?)/",
+        1
+      ),
+      REPLACE(p.cwd, u.directory, "~")
+    )
+  ) AS exception_key
 FROM processes p
   LEFT JOIN processes pp ON p.parent = pp.pid
+  LEFT JOIN users u ON p.uid = u.uid
   LEFT JOIN hash ON p.path = hash.path
 WHERE p.cwd LIKE "%/.%"
-  AND NOT (
-    p.cwd LIKE "%/.local/share%"
-    OR p.cwd LIKE "%/.vscode/extensions%"
-    OR p.cwd LIKE "/Users/%/.%"
-    OR p.cwd LIKE "/home/%/.%"
-    OR p.cwd LIKE "/Library/Apple/System/Library/InstallerSandboxes/.PKInstallSandboxManager-SystemSoftware/%"
-    OR p.cwd LIKE "/tmp/.wine-%/server-%"
-    OR p.name IN ('bindfs', 'wineserver')
-    OR p.path = "/usr/libexec/dirhelper"
-  )
+  AND NOT exception_key IN ("bash,~/go/src", "mysqld,~/.local/share")
+  OR program_name IN ("bindfs")
+  OR dir LIKE "~/go/src/%"
+  OR dir LIKE "~/src/%"
+  OR dir LIKE "~/%/.github%"
