4a7f734c81d946c5f9e54dece6ba801dd2847b80
Thomas Stromberg <t+github@chainguard.dev>
2022-10-14 08:42:10 -0400

    Add metadata, mark as Linux only.

--- detection/privesc/unexpected-privileged-containers.sql
+++ detection/privesc/unexpected-privileged-containers.sql
@@ -1,3 +1,13 @@
+-- Detect the execution of priveleged Docker containers which can be used to escape to the host.
+--
+-- references:
+--   * https://attack.mitre.org/techniques/T1611/
+--
+-- This query works on macOS as well, but is only an in-the-wild security problem on Linux,
+-- where the kernel namespaces can be shared.
+--
+-- platform: linux
+-- tags: ephemeral often
 SELECT
   *
 FROM
