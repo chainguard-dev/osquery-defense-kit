83a8c0d58966ccb76214722f493f4298257e2c6c
Thomas Stromberg <t+github@chainguard.dev>
2022-10-18 11:40:42 -0400

    Improve how we deal with the zfs case

--- detection/evasion/ssh-notty.sql
+++ detection/evasion/ssh-notty.sql
@@ -5,12 +5,9 @@
 --
 -- tags: transient process state
 -- platform: posix
-SELECT
-  *
-FROM
-  (
-    SELECT
-      p.pid,
+SELECT *
+FROM (
+    SELECT p.pid,
       p.name,
       p.cmdline AS cmd,
       cp.name AS child_name,
@@ -18,24 +15,23 @@ FROM
       gcp.name AS grandchild_name,
       gcp.cmdline AS grandchild_cmd,
       GROUP_CONCAT(DISTINCT pof.path) AS open_files
-    FROM
-      processes p
+    FROM processes p
       LEFT JOIN process_open_files pof ON p.pid = pof.pid
       LEFT JOIN processes cp ON p.pid = cp.parent
       LEFT JOIN processes gcp ON cp.pid = gcp.parent
-    WHERE
-      p.name = 'sshd'
-    GROUP BY
-      p.pid
+    WHERE p.name = 'sshd'
+    GROUP BY p.pid
   )
-WHERE
-  (
+WHERE (
     INSTR(cmd, '@notty') > 0
     OR (
       open_files != '/dev/null'
       AND INSTR(open_files, '/dev/ptmx') = 0
     )
   )
-  -- Filter out zfs snapshot reception (false positive)
-  AND child_name != 'zfs'
-  -- I wanted to filter out grandchild_name as well, but for some reason that filtered everything out (joins?)
+
+  -- You must specifically check for NULL here, or risk inadvertently filtering everything out.
+  AND (
+      grandchild_name IS NULL
+      OR grandchild_name != 'zfs'
+  )
