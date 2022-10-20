416bdd8fd1b0f72763021da7c7667647f4c8760e
Thomas Stromberg <t+github@chainguard.dev>
2022-10-20 14:11:19 -0400

    Add broader port exception for Chrome

--- detection/c2/unexpected-talkers-linux.sql
+++ detection/c2/unexpected-talkers-linux.sql
@@ -106,5 +106,12 @@ WHERE
     AND s.protocol = 6
     AND p.euid > 500
   )
+  AND NOT (
+    p.name = 'chrome'
+    AND f.filename = 'chrome'
+    AND s.remote_port > 5000
+    AND s.protocol = 6
+    AND p.euid > 500
+  )
 GROUP BY
   p.cmdline
