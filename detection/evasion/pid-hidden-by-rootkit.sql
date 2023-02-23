a7c2ef97e13ef543a0163c129b2043ae5c166647
Thomas Stromberg <t+github@chainguard.dev>
2023-02-23 17:05:11 -0500

    Add detectors for the reveng_rtkit rootkit

---
+++ detection/evasion/pid-hidden-by-rootkit.sql
@@ -0,0 +1,37 @@
+-- Finds processes that are apparently hidden by a rootkit
+--
+-- references:
+--   * https://attack.mitre.org/techniques/T1014/ (Rootkit)
+--
+-- Confirmed to catch revenge-rtkit
+--
+-- false positives:
+--   * custom kernel modules
+--
+-- tags: persistent kernel state
+-- platform: linux
+
+WITH RECURSIVE cnt(x) AS (
+    SELECT 1
+    UNION ALL
+    SELECT x + 1
+    FROM cnt
+    LIMIT 32768
+)
+SELECT p.*
+FROM cnt
+    JOIN processes p ON x = p.pid
+WHERE x NOT IN (
+        SELECT pid
+        FROM processes
+    )
+    AND p.start_time < (strftime('%s', 'now') - 1) -- Improve how we filter tasks out.
+    -- This is not very precise. What we really want to do is verify that
+    -- this pid is not listed as a task of any other pid
+    AND (
+        p.pgroup = p.pid
+        OR (
+            p.pid = p.parent
+            AND p.threads = 1
+        )
+    )
---
+++ detection/evasion/unusually-tainted-kernel-linux.sql
@@ -0,0 +1,29 @@
+-- Unusually tainted kernel - via a loaded kernel module
+--
+-- references:
+--   * https://attack.mitre.org/techniques/T1014/ (Rootkit)
+--   * https://docs.kernel.org/admin-guide/tainted-kernels.html
+--
+-- Confirmed to catch revenge-rtkit
+--
+-- false positives:
+--   * custom kernel modules
+--
+-- tags: persistent kernel state
+-- platform: linux
+--
+-- 12289 is an unsigned, out of tree, proprietary driver
+-- 4097 is a signed, out of tree, proprietary driver
+SELECT current_value AS value,
+    current_value & 65536 AS is_aux,
+    current_value & 8192 is_unsigned,
+    current_value & 4096 AS out_of_tree,
+    current_value & 512 AS kernel_warning,
+    current_value & 614 AS requested_by_userspace,
+    current_value & 8 AS force_unloaded,
+    current_value & 4 AS out_of_spec,
+    current_value & 2 AS force_loaded,
+    current_value & 1 AS proprietary
+FROM system_controls
+WHERE name = "kernel.tainted"
+    AND current_value NOT IN (0, 12289, 4097)
