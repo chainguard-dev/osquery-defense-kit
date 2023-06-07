349ff58fb2e89affa3ea778f75f410da578af4ac
Thomas Stromberg <t+github@chainguard.dev>
2023-06-07 08:58:02 -0400

    fpr: xfce4, Google Earth, Ubuntu

--- detection/initial_access/sketchy-mounted-diskimage.sql
+++ detection/initial_access/sketchy-mounted-diskimage.sql
@@ -118,6 +118,7 @@ WHERE
       AND f != '/Volumes/brotherwdswML_nonPanel/MacResources'
       AND file.filename NOT LIKE '%.previous'
       AND file.filename NOT LIKE '%.interrupted'
+      AND signature.authority != 'Developer ID Application: Google LLC (EQHXZ8M8AV)'
       AND file.filename NOT LIKE '%.backup'
     ) --   7. Volumes containing a top-level symlink to something other than /Applications, such as yWnBJLaF (1302.app)
     OR (
