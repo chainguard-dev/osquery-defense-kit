c9ae0805e29e3b4268ff946eeeadc55bf4d6e20b
egibs <20933572+egibs@users.noreply.github.com>
2024-07-18 07:24:14 -0500

    Add exceptions for Docker's kubectl, ngrok, SAFEQ, and Zed
    Signed-off-by: egibs <20933572+egibs@users.noreply.github.com>

--- detection/c2/unexpected-talkers-macos.sql
+++ detection/c2/unexpected-talkers-macos.sql
@@ -194,7 +194,9 @@ WHERE pos.protocol > 0
     '500,6,993,thunderbird,thunderbird,Developer ID Application: Mozilla Corporation (43AQ936H96),org.mozilla.thunderbird',
     '500,6,995,KakaoTalk,KakaoTalk,Apple Mac OS Application Signing,com.kakao.KakaoTalkMac',
     '0,6,853,at.obdev.littlesnitch.networkextension,at.obdev.littlesnitch.networkextension,0u,0g',
-    '500,6,21,Cyberduck,Cyberduck,Developer ID Application: David Kocher (G69SCX94XU),ch.sudo.cyberduck'
+    '500,6,21,Cyberduck,Cyberduck,Developer ID Application: David Kocher (G69SCX94XU),ch.sudo.cyberduck',
+    '500,6,7881,zed,zed,Developer ID Application: Zed Industries, Inc. (MQ55VZLNZQ),dev.zed.Zed',
+    '0,6,7300,safeqclientcore,safeqclientcore,Developer ID Application: Y Soft Corporation, a.s. (3CPED8WGS9),safeqclientcore'
   ) -- Useful for unsigned binaries
   AND NOT alt_exception_key IN (
     '0,6,80,tailscaled,tailscaled,500u,80g',
